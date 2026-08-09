f31d97677f9cfcd50cbea1a98b83b616a5b36b9e
Jatan Kumar <jatankumarjk806@gmail.com>
2026-08-09 21:09:10 +0530

    Time: 1 ms (29.04%), Space: 9.4 MB (84.97%) - LeetHub

--- 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
+++ 4006-count-valid-prefixes/4006-count-valid-prefixes.cpp
@@ -1,32 +1,19 @@
 class Solution {
 public:
-    bool helper(string sub){
-        int countZero=0;
-        int countOne=0;
-        for(char ch: sub){
-            if(ch=='1'){
-                countOne++;
-            }
-            if(ch=='0'){
-                countZero++;
-            }
-        }
-        if(countZero==0 && countOne==1 || countZero==1 && countOne==0 ){
-            return 1;
-        }
-        if(countZero+1==countOne || countZero==countOne+1 || countZero==countOne ){
-            return 1;
-        }  
-        return 0;
-    }
     int countValidPrefixes(string s) {
+        int n = s.length();
         int ans=0;
-        for(int i=0;i<s.size();i++){
-            string sub=s.substr(0,i+1);
-            if(helper(sub)){
-                ans++;
-            }
+
+        int count0 = 0;
+        int count1 = 0;
+
+        for(int i=0; i<n; i++){
+            if(s[i] == '0') count0++;
+            if(s[i] == '1') count1++;
+
+            if(abs(count0-count1) <= 1) ans++;
         }
+
         return ans;
     }
 };
